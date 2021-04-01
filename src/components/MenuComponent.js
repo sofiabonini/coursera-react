ea999dd7125c09f20fe11a6b591bbb54bed635ba
Sofia Bonini <boninisofia14@gmail.com>
2021-04-01 16:36:00 +0200

    react router & SPA part 1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -19,18 +19,18 @@ import { Card, CardImg, CardImgOverlay,
         const menu = props.dishes.map((dish) => {
             return (
                 <div className="col-12 col-md-5 m-1"  key={dish.id}>
-                    <RenderMenuItem dish={dish} onClick={props.onClick} />
-                </div>
-            );
-        });
-
-        return (
-            <div className="container">
-                <div className="row">
-                    {menu}
-                </div>
+                <RenderMenuItem dish={dish} onClick={props.onClick} />
             </div>
         );
-    }
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                {menu}
+            </div>
+        </div>
+    );
+}
 
 export default Menu;
