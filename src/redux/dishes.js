03f5be46be5eeca001298409d3d74ff7d880c39e
Sofia Bonini <boninisofia14@gmail.com>
2021-04-02 14:37:39 +0200

    redux thunk

--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,8 +1,24 @@
-import { DISHES } from '../shared/dishes';
+import * as ActionTypes from './ActionTypes';
 
-export const Dishes = (state = DISHES, action) => {
+export const Dishes = (state = {
+    isLoading: true,
+    errMess: null,
+    dishes:[]
+
+}, action) => {
     switch (action.type) {
+        case ActionTypes.ADD_DISHES:
+            // action.payload carries 'dishes' information
+            return {...state, isLoading: false, errMess: null, dishes: action.payload};
+
+        case ActionTypes.DISHES_LOADING:
+            // with "..." i change the state object
+            return {...state, isLoading: true, errMess: null, dishes: []}
+
+        case ActionTypes.DISHES_FAILED:
+            return {...state, isLoading: false, errMess: action.payload};
+
         default:
-          return state;
-      }
+            return state;
+    }
 };
